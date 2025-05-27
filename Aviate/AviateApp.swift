cea6ea9167622a29847d36f8ba473aaa87a137aa
Achitha Sandaruwan <achitha.emedia@gmail.com>
2025-05-27 23:58:48 +0530

    Initial Commit

---
+++ Aviate/AviateApp.swift
@@ -0,0 +1,20 @@
+//
+//  AviateApp.swift
+//  Aviate
+//
+//  Created by Admin on 2025-05-27.
+//
+
+import SwiftUI
+
+@main
+struct AviateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
